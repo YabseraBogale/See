00df3cf39d09854de17ae4c2add5644813804a34
YabseraBogale <96526984+YabseraBogale@users.noreply.github.com>
2022-04-12 12:33:23 +0300

    Add files via upload
    use for loop well have buffer time problem. This is an as an idea test.

---
+++ time.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main(){
+	
+	here:
+
+	for(int hr=0;hr<13;hr++){
+		
+		for(int min=0;min<61;min++){
+		
+			for(int sec=0;sec<61;sec++){
+				
+				
+				
+				
+				for(int msec=0;msec<1000;msec++){
+					
+					cout<<hr<<":"<<min<<":"<<sec<<endl;
+					system("clear");
+					
+					
+				}
+				
+			
+			}
+		
+		
+		
+		
+		
+		}
+		
+		
+		
+		
+		
+	}
+
+
+	goto here;
+
+
+
+
+
+
+
+
+
+	return 0;
+}
