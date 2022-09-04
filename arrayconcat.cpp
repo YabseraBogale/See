454bd9660c529f534966e40d0db75c4a1b5bfe1e
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2022-09-04 19:46:41 +0300

    Add files via upload

---
+++ arrayconcat.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int *arrcon(int arr1[],int arr2[]){
+	int arr1s=sizeof(arr1)/4;
+	int arr2s=sizeof(arr2)/4;
+	if(arr1s>arr2s){
+		
+		
+	}
+	return arr1;
+	}
+int main(){
+	int arr[2]={1,2};
+	int arr1[3]={3,4};
+	cout<<arrcon(arr,arr1);
+	
+	return 0;
+	}
