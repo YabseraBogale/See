d20fa379d9d175ea689942c1ba54849f4c6a680f
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2022-11-12 16:21:49 +0300

    Add files via upload

---
+++ InsertionSort.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+	int *arr,i,j,k,temp,no;
+	cout<<"How many elements to be inserted into the list";
+	cin>>n;
+	return 0;
+	}
+
