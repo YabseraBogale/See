9cdec6e1cd65e11a18eee4d61639fe9585229e36
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-01-01 18:14:19 +0300

    Add files via upload

---
+++ Wine_withStructArray.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+struct WineArray{
+	int Date;
+	string wine_name;
+	//index 0 dd index 1 mm index 2 yy
+	int wine_made_year[3];
+	float price;
+	int sold_day[3];
+	string wine_type[3]={"red wine","white wine","wine tedj"};
+	string wine_manfauture;
+	int sales_frequency;
+	};
+int main(){
+	return 0;
+	}
