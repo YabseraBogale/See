78c852f49a5952ae570bbee40bce8277ff19e0f6
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-05-01 16:29:07 +0300

    Add files via upload

---
+++ Testmain.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+double standardRoomPrices[] = {182.16, 202, 202, 202, 170.74, 170.74, 170.74, 227, 227, 189.75, 189.75, 189.75, 170.79, 189, 210};
+double executiveRoomPrices[] = {250.47, 278, 278, 278, 239.1, 239.1, 239.1, 303.6, 303.6, 265, 265, 265, 240, 264, 290};
+double presidentialRoomPrices[] = {523.71, 578, 578, 578, 512, 512, 512, 607, 607, 570, 570, 570, 512, 550, 595};
+double kingBedPrices[] = {200, 220, 220, 220, 210, 210, 200, 230, 230, 220, 220, 220, 220, 220, 240};
+double deluxeKingBedPrices[] = {250, 285, 285, 285, 270, 270, 260, 299, 299, 285, 285, 285, 285, 285, 320};
+double hyattPresidentialRoomPrices[] = {2200, 2220, 2220, 2220, 2200, 2200, 2200, 2220, 2220, 2200, 2200, 2200, 2200, 2200, 2240};
+
+double DayAndPrice(int day,double roomchoice,int nights){
+	
+	double Price=0;
+	if(day==31){
+		switch(roomchoice){
+			case 1:
+				
+		}
+	}
+	
+	return Price;
+	}
+
+int main(){
+	
+	return 0;
+	}
