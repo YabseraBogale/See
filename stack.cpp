a5f1af09c04b4b47ce01f783da4029220a3cdf32
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-05-05 18:57:32 +0300

    Add files via upload

---
+++ stack.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+    stack<string> Names;
+    Names.push("Yabsera");
+    Names.push("Bogale");
+    cout<<"TOP: "<<Names.top()<<endl;
+}
