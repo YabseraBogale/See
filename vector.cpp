0197c14e834df8c52700b228d7d672d30200c9a6
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-05-05 18:58:20 +0300

    Add files via upload

---
+++ vector.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> numbers={1,2,3,4,5};
+    for(int i=0;i<5;i++){
+        cout<<numbers[i]<<endl;
+    }
+    return 0;
+}
