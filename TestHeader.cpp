03ee3eb55f725148ee85fdfe4829aefa1290f16e
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-06-16 19:52:27 +0300

    Add files via upload

---
+++ TestHeader.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include "sum.h"
+#include "mul.h"
+using namespace std;
+ 
+// Driver Code
+int main()
+{
+ 
+    // Given two numbers
+    int a = 13, b = 22;
+ 
+    // Function declared in header
+    // file to find the sum
+    cout<<"Sum is: "<<product()<<endl;
+}
