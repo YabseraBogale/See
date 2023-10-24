ea258244c31c00d394b34acbcb5807e5bb7683cc
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-24 19:14:33 +0300

    mother fucker

--- quick.cpp
+++ quick.cpp
@@ -3,17 +3,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arrayEven[]={1,2,3,4,5,6};
+    int arrayEven[]={1,2,3,4,5,6,7,8};
     int arrayOdd[]={1,2,3,4,5,6,7};
     int even = (sizeof(arrayEven)/sizeof(arrayEven[0]))/2;
     int odd = (sizeof(arrayOdd)/sizeof(arrayOdd[0]))/2 +1;
-    int i=1;
-    int k=even;
-    while(i<=even){
-        
-        cout<<endl;
+    int counter=even;
+    for(int i=1;i<=even;i++){
+        int j=i*2;
+       
         i+=1;
     }
+    
     
     return 0;
 }
