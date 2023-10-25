b1fa35b17fd99880fdb8d341730c6c58c1b5ae28
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-25 17:41:52 +0300

    ol

--- qq.cpp
+++ qq.cpp
@@ -2,8 +2,33 @@
 // g++ -o quick qq.cpp && ./qq
 #include<iostream>
 using namespace std;
-
+int arrayFilter(int a[], int begin,int end){
+    int sum=0;
+    while(begin<=end){
+        sum+=a[begin-1];
+        begin+=1;
+    }
+    return sum;    
+}
+string theNumberCanBeMadeWithTheArray(int a[],int number){
+        int odd=(sizeof(a)/sizeof(a[0]))/2 +1;
+        int counter=odd;
+        for(int i=1;i<=odd;i++){
+            int j=odd+i;
+            int sum=0;
+            if(counter>=0){
+                sum=arrayFilter(a,counter,j);
+                counter-=1;
+            }
+            if(sum==number){
+                cout<<sum<<endl;
+                return "true";
+            }
+        
+        }
+        return "false";
+    }
 int main(){
-    cout<<"OO"<<endl;
+    
     return 0;
 }
