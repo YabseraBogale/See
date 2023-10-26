37f91d7341ead6c568d3ebebc2636c4a69b7fcd4
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-26 16:45:43 +0300

    ok

--- testgit.cpp
+++ testgit.cpp
@@ -4,13 +4,10 @@
 #include <stdlib.h>
 using namespace std;
 int main(){
-	system("git status");
-	system("git add .");
-	system("git commit -m 'ok'");
-	system("git push");
+	
 	char buffer[128];
 	string result = "";
-    FILE *fp=popen("git status","r");
+    FILE *fp=popen("git status && git add . && git commit -m 'ok' && git push","r");
     while (!feof(fp)) {
 
       // use buffer to read and add to result
