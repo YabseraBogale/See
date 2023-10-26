2f9794143d3e8fcd2b636eb72768e8e3e2be3b5d
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-26 16:38:24 +0300

    ok

--- testgit.cpp
+++ testgit.cpp
@@ -4,9 +4,15 @@
 #include <stdlib.h>
 using namespace std;
 int main(){
-    system("git status");
-    system("git add .");
-    system("git commit -m 'ok'");
-    system("git push");
+	system("git status");
+	system("git add .");
+	system("git commit -m 'ok'");
+	system("git push");
+	int status;
+    FILE *fp=popen("git status","r");
+    status=pclose(fp);
+    if(status==-1){
+		cout<<"error"<<endl;
+	}
     return 0;
 }
