1d2f7937aba745095d3ba3b1fb052c2e82a5582d
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-26 16:44:39 +0300

    ok

--- testgit.cpp
+++ testgit.cpp
@@ -8,11 +8,17 @@ int main(){
 	system("git add .");
 	system("git commit -m 'ok'");
 	system("git push");
-	int status;
+	char buffer[128];
+	string result = "";
     FILE *fp=popen("git status","r");
-    status=pclose(fp);
-    if(status==-1){
-		cout<<"error"<<endl;
-	}
+    while (!feof(fp)) {
+
+      // use buffer to read and add to result
+      if (fgets(buffer, 128, fp) != NULL)
+         result += buffer;
+   }
+   system("clear");
+   cout<<result<<endl;
+    
     return 0;
 }
